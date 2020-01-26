bce72d293f23813b266fb5fa101c3c080e1d62c1
Alexis Perumal <>
2020-01-26 15:31:24 -0800

    Started adding queries to the SQL script.

--- EmployeeSQL/sql-challenge-alexis.sql
+++ EmployeeSQL/sql-challenge-alexis.sql
@@ -105,9 +105,14 @@ SELECT * FROM titles LIMIT 10;
 
 -- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 
+SELECT employees.emp_no, last_name, first_name, gender, salaries.salary, from_date, to_date FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no
+ORDER BY emp_no ASC
+LIMIT 100;
 
 -- 2. List employees who were hired in 1986.
-
+SELECT emp_no, last_name, first_name, hire_date FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986_12-31' ORDER BY emp_no LIMIT 10;
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 
@@ -125,3 +130,4 @@ SELECT * FROM titles LIMIT 10;
 
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
